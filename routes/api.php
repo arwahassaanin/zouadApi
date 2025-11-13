45f8f2ed4ee626e7f827a3a981f1ec1d35e61fe7
ArwaNabilHassanin <arwanabil002@gmail.com>
2025-11-13 10:34:48 +0200

    ph

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\EmailVerficationController;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 
 // Route::get('/user', function (Request $request) {
@@ -14,4 +15,13 @@ use App\Http\Controllers\Api\EmailVerficationController;
 // Route::post('/test-upload', [ProfileController::class, 'uploadTest']);
 //  Route::get('/test-cloudinary', [ProfileController::class, 'checkCloudinary']);
 
+Route::post('/test-upload', function (Request $request) {
+    if ($request->hasFile('photo')) {
+        $imageUrl = Cloudinary::upload($request->file('photo')->getRealPath())
+                    ->getSecurePath();
+        return response()->json(['url' => $imageUrl]);
+    } else {
+        return response()->json(['error' => 'No file uploaded'], 400);
+    }
+});
 
